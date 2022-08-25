a5cba2bc4a8c74541603f2646a8d29dd7ee097f5
Thuan Luu <thuanrt123@gmail.com>
2022-08-25 12:18:19 +0700

    create layouts for login and registration features

--- app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
@@ -2,15 +2,26 @@ package com.example.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
     }
+
+    private void setListeners(){
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+
 }
